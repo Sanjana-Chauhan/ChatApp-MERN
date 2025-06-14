a48ef347fc7e385645f8ffd59a4d1ab08bd9135a
Sanjana <chauhansanjana965@gmail.com>
2025-06-14 23:14:57 +0530

    UserList and system message emiited

--- Backend/app.js
+++ Backend/app.js
@@ -1,33 +1,17 @@
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
-import { Server } from 'socket.io';
+// server.js
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { setupSocket } from "./socket.js"; // <-- import from socket file
 
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
-});
-
-io.on('connection', (socket) => {
-  console.log(` User connected: ${socket.id}`);
-
-  socket.on('chat-message', (data) => {
-    console.log(`Message received: ${data}`);
-    socket.broadcast.emit('chat-message', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log(`User disconnected: ${socket.id}`);
-  });
-});
+// Pass server to the Socket.IO setup
+setupSocket(server);
 
 server.listen(5000, () => {
-  console.log(' Server running on http://localhost:5000');
+  console.log("Server running on http://localhost:5000");
 });
--- Backend/socket.js
+++ Backend/socket.js
@@ -1,16 +1,48 @@
 // socket.js
-export function setupSocket(io) {
-  io.on("connection", (socket) => {
-    console.log(" A user connected:", socket.id);
+import { Server } from "socket.io";
 
-    socket.on("chatMessage", (msg) => {
-      console.log(" Message received:", msg);
-      // Broadcast to all clients
-      io.emit("chatMessage", msg);
+let users = []; // global array to track users
+
+export const setupSocket = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("set_nickname", (nickname) => {
+      socket.nickname = nickname;
+
+      // Avoid duplicates: remove if already present (edge case)
+      users = users.filter((u) => u.id !== socket.id);
+   
+      users.push({ id: socket.id, name: nickname ,online:true});
+
+      io.emit("user_list", users); // broadcast full list
+      io.emit("user_joined", nickname); // system message
+    });
+
+    socket.on("chat-message", (data) => {
+      const sender = socket.nickname || "Anonymous";
+      const messageData = {
+        ...data,
+        from: sender,
+      };
+      socket.broadcast.emit("chat-message", messageData);
     });
 
     socket.on("disconnect", () => {
-      console.log(" User disconnected:", socket.id);
+      const nickname = socket.nickname || "Someone";
+
+      // Remove from users list
+      users = users.filter((u) => u.id !== socket.id);
+
+      io.emit("user_list", users); // update the client list
+      socket.broadcast.emit("user_left", `${nickname} left the chat`);
     });
   });
-}
+};
